f774b9462895ba4fe3a8a42c2df2ea27914270bf
EAMehdi <mehdi.el-ayadi@dauphine.eu>
2023-06-24 23:09:12 +0000

    Update Test with pytest

--- crypt_component/test.py
+++ crypt_component/test.py
@@ -1,11 +1,24 @@
+import pytest
 import crypt_message as crypt
 
-key = "secret"
-message = "Hello World!"
-message_alt = "Hello World!"
-result = crypt.hmac_sha512(key, message)
+def test_hmac_sha512_valid():
+    key = "secret"
+    message = "Hello, World!"
+    assert crypt.hmac_sha512(key, message) == '851caed63934ad1c9a03aef23ba2b84f224bdff4f5148efc57d95f9ae80ca9db2e98bc4c709a529eb1b7234a1ac2e381d28e0eb1efa090bb19613f5c124b6d5b'
 
-print(f'Le message original est : {message}')
-print(f'La clé est : {key}')
-print(f'Message chiffré : {result}')
+def test_hmac_sha512_empty_key():
+    message = "Hello, World!"
+    with pytest.raises(ValueError):
+        crypt.hmac_sha512('', message)
+
+def test_hmac_sha512_wrong_key_type():
+    message = "Hello, World!"
+    with pytest.raises(TypeError):
+        crypt.hmac_sha512(123, message)
+
+def test_hmac_sha512_wrong_message_type():
+    key = "secret"
+    with pytest.raises(TypeError):
+        crypt.hmac_sha512(key, 123)
+
 
